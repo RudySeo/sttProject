82916a9507ea2f2b1b5d8600e257feb4924da2b0
RudySeo <gun100491@gmail.com>
2025-07-30 23:49:48 +0900

    feat(api) 비동기 호출

--- app/router/api.py
+++ app/router/api.py
@@ -1,35 +1,29 @@
 # app/router/api.py
 
-from fastapi import APIRouter, UploadFile, File, HTTPException
+from app.graph.runner.sttSummaryRunner import runGraph
+from fastapi import APIRouter, UploadFile, File
 import uuid, os, shutil
-from app.service.sttService import transcribe_audio_with_api, validate_api_key
 
 router = APIRouter()
 
-UPLOAD_DIR = "uploads"
-os.makedirs(UPLOAD_DIR, exist_ok=True)
-
 
 @router.post("/upload-audio")
-async def upload_audio(file: UploadFile = File(...)):
-    # API 키 유효성 확인
-    if not validate_api_key():
-        raise HTTPException(
-            status_code=401, detail="유효하지 않은 OpenAI API 키입니다."
-        )
+async def uploadAudio(file: UploadFile = File(...)):
+    """
+    오디오 파일을 업로드 받아서 서버에 저장 후
+    LangGraph를 통해 STT 및 요약을 수행하는 API입니다.
+    """
 
-    # 고유한 파일명 생성
-    file_id = str(uuid.uuid4())
-    file_path = os.path.join(UPLOAD_DIR, f"{file_id}.wav")
+    # 업로드 파일을 위한 디렉토리 설정
+    UPLOAD_DIR = "uploads"
+    fileId = str(uuid.uuid4())
+    uploadPath = os.path.join(UPLOAD_DIR, f"{fileId}.wav")
 
-    # 파일 저장
-    with open(file_path, "wb") as buffer:
+    #  업로드 파일을 로컬에 저장
+    with open(uploadPath, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    # Whisper API로 STT 처리
-    try:
-        text = transcribe_audio_with_api(file_path)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"STT 처리 실패: {e}")
+    #  LangGraph를 실행하고 결과 받아오기
+    result = await runGraph(uploadPath)
 
-    return {"message": "STT 성공", "text": text}
+    return {"result": result}
