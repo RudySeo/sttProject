0f4b1db6e504f65e446f181fe232aa8b76f69a9e
RudySeo <gun100491@gmail.com>
2025-07-31 21:33:49 +0900

    feat(keyword) keywordNode 추가

--- app/graph/edge/sttSummaryEdge.py
+++ app/graph/edge/sttSummaryEdge.py
@@ -1,5 +1,6 @@
+from app.graph.node.keywordNode import keywordNode
 from langgraph.graph import StateGraph
-from typing import TypedDict
+from typing import TypedDict, List
 from app.graph.node.sttNode import sttNode
 from app.graph.node.summarizerNode import summarizerNode
 
@@ -8,6 +9,7 @@ class GraphState(TypedDict):
     audioFilePath: str  # 오디오 파일경로
     transcript: str  # STT 텍스트 결과
     summary: str  # 요약된 텍스트 결과
+    keyword: List[str]  # 변경!  # 키워드
 
 
 def buildSttSummaryGraph():
@@ -16,9 +18,11 @@ def buildSttSummaryGraph():
     # 노드 등록
     graph.add_node("stt", sttNode)
     graph.add_node("summarizer", summarizerNode)
+    graph.add_node("keyword", keywordNode)
 
     # 노드 연결 흐름 구성
     graph.set_entry_point("stt")
     graph.add_edge("stt", "summarizer")
-    graph.set_finish_point("summarizer")
+    graph.add_edge("summarizer", "keyword")
+    graph.set_finish_point("keyword")
     return graph.compile()
